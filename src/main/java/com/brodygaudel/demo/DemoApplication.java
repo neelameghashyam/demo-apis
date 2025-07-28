3dab6c58172118fcaf3897b210900cf2e17167af
Shyam <neelameghashyam@gmail.com>
2025-07-28 11:33:03 +0530

    db -> freedb

--- src/main/java/com/brodygaudel/demo/DemoApplication.java
+++ src/main/java/com/brodygaudel/demo/DemoApplication.java
@@ -2,12 +2,12 @@ package com.brodygaudel.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.brodygaudel.demo.entity")
 public class DemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
